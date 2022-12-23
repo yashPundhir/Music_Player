7fabefd35bae1292812d42f5e04e484423ed8941
yashPundhir <yash.pundhir.prof@gmail.com>
2022-12-23 14:48:42 +0530

    Play/Pause functionality of music player is added

--- script.js
+++ script.js
@@ -3,3 +3,44 @@ let valueContainer = document.querySelector(".value-container");
 let progressStartValue = 0;
 let progressEndValue = 123;
 let speed = 1000;
+// let progress = setInterval(() => {
+// 	progressStartValue = progressStartValue + 1;
+// 	// valueContainer.textContent = `${progressStartValue}%`;
+// 	{
+// 		progressBar.style.background = `conic-gradient(
+//       #b0b0b0 ${(progressStartValue + 0.5) * 2.927}deg,
+//       #252525 ${(progressStartValue + 0.5) * 2.927}deg
+//   )`;
+// 	}
+// 	if (progressStartValue == progressEndValue) {
+// 		clearInterval(progress);
+// 	}
+// }, speed);
+let song = document.querySelector(".song audio");
+let play = document.querySelector(".play");
+let pause = document.querySelector(".pause");
+function playSong() {
+	song.play();
+	play.style.display = "none";
+	pause.style.display = "flex";
+	pause.style.marginLeft = "-2.5px";
+	// let progress = setInterval(() => {
+	// 	progressStartValue = progressStartValue + 1;
+	// 	valueContainer.textContent = `${progressStartValue}%`;
+	// 	{
+	// 		progressBar.style.background = `conic-gradient(
+	// 			#b0b0b0 ${(progressStartValue + 0.5) * 2.927}deg,
+	// 			#252525 ${(progressStartValue + 0.5) * 2.927}deg
+	// 	)`;
+	// 	}
+	// 	if (progressStartValue == progressEndValue) {
+	// 		clearInterval(progress);
+	// 	}
+	// }, speed);
+}
+function pauseSong() {
+	song.pause();
+	play.style.display = "block";
+	pause.style.display = "none";
+	play.style.marginLeft = "2.5px";
+}
